433ae7859eccde3edcf346a0ad63a760c8c04e6d
Sabatie <gsabatie@me.com>
2018-01-04 22:43:17 +0100

    added chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatCardModule, MatMenuModule, MatSidenavModule, MatSelectModule,  MatCheckboxModule, MatToolbarModule } from "@angular/material";
+import { MatCardModule, MatMenuModule, MatSidenavModule, MatSelectModule,  MatCheckboxModule, MatToolbarModule } from '@angular/material';
 import {ChartistModule} from 'ng-chartist';
 
 import { AppComponent } from './app.component';
